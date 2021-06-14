13cff122c4cc7183d8ab93f45842f188e3b6f3bf
anthonycc2 <anthonycc2@users.noreply.github.com>
2021-06-14 02:48:44 -0300

    4o commit - CORS

--- src/main/java/org/cesar/CesarLog/CesarLogApplication.java
+++ src/main/java/org/cesar/CesarLog/CesarLogApplication.java
@@ -18,7 +18,8 @@ public class CesarLogApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("GET", "POST", "PUT", "DELETE");
+				//registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("GET", "POST", "PUT", "DELETE");
+				registry.addMapping("/**").allowedOrigins("https://cesarlog.netlify.app").allowedMethods("GET", "POST", "PUT", "DELETE");
 			}
 		};
 	}
